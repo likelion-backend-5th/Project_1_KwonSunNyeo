20fca0f59866c5cad2e9bc1c761ece0063dbd85d
sssssn <doitforyourself0603@gmail.com>
2023-07-03 18:37:53 +0900

    ✨ 응답 형식 구현

--- src/main/java/com/likelion/market/controller/ItemController.java
+++ src/main/java/com/likelion/market/controller/ItemController.java
@@ -1,10 +1,14 @@
 package com.likelion.market.controller;
 
 import com.likelion.market.dto.ItemDto;
+import com.likelion.market.dto.MessageResponseDto;
+import com.likelion.market.dto.ResponseDto;
 import com.likelion.market.service.ItemService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,15 +21,20 @@ public class ItemController {
     // 물품 정보 - 등록
     // POST /items
     @PostMapping
-    public ItemDto create(@RequestBody ItemDto dto) {
-        return service.createItem(dto);
+    public ResponseEntity<ResponseDto> create(@RequestBody ItemDto dto) {
+        service.createItem(dto);
+        ResponseDto response = new ResponseDto();
+        response.setMessage("등록이 완료되었습니다.");
+        response.setStatus(200);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 물품 정보 - 단일 조회
     // GET /items/{itemId}
     @GetMapping("/{id}")
-    public ItemDto read(@PathVariable("id") Long id) {
-        return service.readItem(id);
+    public ResponseEntity<ItemDto> read(@PathVariable("id") Long id) {
+        ItemDto item = service.readItem(id);
+        return new ResponseEntity<>(item, HttpStatus.OK);
     }
 
     // 물품 정보 - 페이지 단위 조회
@@ -48,20 +57,26 @@ public class ItemController {
     // 물품 정보 - 수정
     // PUT /items/{itemId}
     @PutMapping("/{id}")
-    public ItemDto update(
+    public ResponseEntity<MessageResponseDto> update(
             @PathVariable("id") Long id,
             @RequestBody ItemDto dto
     ) {
-        return service.updateItem(id, dto);
+        service.updateItem(id, dto);
+        MessageResponseDto response = new MessageResponseDto();
+        response.setMessage("물품이 수정되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 물품 정보 - 삭제
     // DELETE /items/{itemId}
     @DeleteMapping("/{id}")
-    public void delete(
+    public ResponseEntity<MessageResponseDto> delete(
             @PathVariable("id") Long id,
             @RequestBody ItemDto dto
     ) {
         service.deleteItem(id, dto);
+        MessageResponseDto response = new MessageResponseDto();
+        response.setMessage("물품을 삭제했습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
