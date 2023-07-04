202c250059868fc2c8168d8d924677316c5fa7df
sssssn <doitforyourself0603@gmail.com>
2023-07-04 13:52:07 +0900

    ♻️ 응답 형식 구현

--- src/main/java/com/likelion/market/controller/CommentController.java
+++ src/main/java/com/likelion/market/controller/CommentController.java
@@ -1,10 +1,14 @@
 package com.likelion.market.controller;
 
 import com.likelion.market.dto.CommentDto;
+import com.likelion.market.dto.MessageResponseDto;
+import com.likelion.market.dto.ResponseDto;
 import com.likelion.market.service.CommentService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @Slf4j
@@ -17,11 +21,15 @@ public class CommentController {
     // 물품 댓글 - 등록
     // POST /items/{itemId}/comments
     @PostMapping
-    public CommentDto create(
+    public ResponseEntity<ResponseDto> create(
             @PathVariable("itemId") Long itemId,
             @RequestBody CommentDto dto
     ) {
-        return service.createComment(itemId, dto);
+        service.createComment(itemId, dto);
+        ResponseDto response = new ResponseDto();
+        response.setMessage("댓글이 등록되었습니다.");
+        response.setStatus(200);
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 물품 댓글 - 페이지 단위 조회
@@ -47,33 +55,42 @@ public class CommentController {
     // 물품 댓글 - 수정
     // PUT /items/{itemId}/comments/{commentId}
     @PutMapping("/{commentId}")
-    public CommentDto update(
+    public ResponseEntity<MessageResponseDto> update(
             @PathVariable("itemId") Long itemId,
             @PathVariable("commentId") Long commentId,
             @RequestBody CommentDto dto
     ) {
-        return service.updateComment(itemId, commentId, dto);
+        service.updateComment(itemId, commentId, dto);
+        MessageResponseDto response = new MessageResponseDto();
+        response.setMessage("댓글이 수정되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 물품 댓글 - 답글
     // PUT /items/{itemId}/comments/{commentId}/reply
     @PutMapping("/{commentId}/reply")
-    public CommentDto updateReply(
+    public ResponseEntity<MessageResponseDto> updateReply(
             @PathVariable("itemId") Long itemId,
             @PathVariable("commentId") Long commentId,
             @RequestBody CommentDto dto
     ) {
-        return service.updateReply(itemId, commentId, dto);
+        service.updateReply(itemId, commentId, dto);
+        MessageResponseDto response = new MessageResponseDto();
+        response.setMessage("댓글에 답변이 추가되었습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     // 물품 댓글 - 삭제
     // DELETE /items/{itemId}/comments/{commentId}
     @DeleteMapping("/{commentId}")
-    public void delete(
+    public ResponseEntity<MessageResponseDto> delete(
             @PathVariable("itemId") Long itemId,
             @PathVariable("commentId") Long commentId,
             @RequestBody CommentDto dto
     ) {
         service.deleteComment(itemId, commentId, dto);
+        MessageResponseDto response = new MessageResponseDto();
+        response.setMessage("댓글을 삭제했습니다.");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
