60a60e5c0b1da0453c193be772b3dfe2f33a6592
sssssn <doitforyourself0603@gmail.com>
2023-07-26 20:16:34 +0900

    ♻️ 사용자 인증 정보 확인 기능 추가

--- src/main/java/com/likelion/market/controller/UserController.java
+++ src/main/java/com/likelion/market/controller/UserController.java
@@ -1,9 +1,16 @@
 package com.likelion.market.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Slf4j
 @Controller
@@ -17,7 +24,13 @@ public class UserController {
 
     // 마이 프로필 페이지 반환
     @GetMapping("/my-profile")
-    public String myProfile() {
+    public String myProfile(
+            Authentication authentication
+    ) {
+        // 현재 접속중인 아이디 출력
+        log.info(authentication.getName());
+        log.info(((User) authentication.getPrincipal()).getUsername());
+        log.info(SecurityContextHolder.getContext().getAuthentication().getName());
         return "my-profile";
     }
 
@@ -25,5 +38,36 @@ public class UserController {
     @GetMapping("/register")
     public String registerForm() {
         return "register-form";
+    }
+
+    private final UserDetailsManager manager;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserController(
+            UserDetailsManager manager,
+            PasswordEncoder passwordEncoder
+    ) {
+        this.manager = manager;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    // 회원가입 요청 처리
+    @PostMapping("/register")
+    public String registerPost(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            @RequestParam("password-check") String passwordCheck
+    ) {
+        // 입력받은 비밀번호와 비밀번호 확인 값이 일치하는 경우, 사용자 생성 및 로그인 페이지로 이동
+        if (password.equals(passwordCheck)) {
+            log.info("password match!");
+            manager.createUser(User.withUsername(username)
+                    .password(passwordEncoder.encode(password))
+                    .build());
+            return "redirect:/users/login";
+        }
+        // 일치하지 않은 경우, 회원가입 페이지로 이동
+        log.warn("password does not match...");
+        return "redirect:/users/register?error";
     }
 }
