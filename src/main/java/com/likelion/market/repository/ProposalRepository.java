e8090b3074e9c0e95ece84cf0dd869a6ccf2a7c9
sssssn <doitforyourself0603@gmail.com>
2023-07-27 23:16:08 +0900

    🎨 Service, Repository 수정
    - 사용자 검증 방식 변경 및 예외 메시지 개선
    - findByIdAndUser 메서드 추가

--- src/main/java/com/likelion/market/repository/ProposalRepository.java
+++ src/main/java/com/likelion/market/repository/ProposalRepository.java
@@ -2,13 +2,16 @@ package com.likelion.market.repository;
 
 import com.likelion.market.entity.ProposalEntity;
 import com.likelion.market.entity.ProposalStatus;
+import com.likelion.market.entity.UserEntity;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ProposalRepository extends JpaRepository<ProposalEntity, Long> {
-    Page<ProposalEntity> findByItemIdAndWriterAndPassword(Long itemId, String writer, String password, Pageable pageable);
+    Page<ProposalEntity> findByItemIdAndUserAndPassword(Long itemId, String writer, String password, Pageable pageable);
     List<ProposalEntity> findAllByItemIdAndStatus(Long itemId, ProposalStatus status);
+    Optional<ProposalEntity> findByIdAndUser(Long id, UserEntity user);
 }
